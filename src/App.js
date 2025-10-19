54f12582908c5f17bab054fb8f920e80a9e42340
Bala Sudalaimuthu <bala@Balas-MacBook-Air.local>
2025-10-19 19:09:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="iframe-container">
+        <iframe
+          src="https://my.spline.design/nexbotrobotcharacterconcept-eOVKTcn4uy1c8eqDjRWnlqkQ/"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+          title="Spline 3D Model"
+        ></iframe>
+      </div>
+
+      <div className="content">
+        <h1>Welcome to NexBot</h1>
+        <p>This is your 3D robot concept rendered using Spline.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
